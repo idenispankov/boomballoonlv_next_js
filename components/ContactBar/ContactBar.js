bd02227b554389aae44010dba7eea14f66d42e0d
Denis Pankov <idenispankov@gmail.com>
2022-07-02 14:33:45 -0700

    test mobile

--- components/ContactBar/ContactBar.js
+++ components/ContactBar/ContactBar.js
@@ -1,4 +1,6 @@
 import styles from './ContactBar.module.css';
+import Image from 'next/image';
+import Logo from '../../images/logo.svg';
 import { AiOutlineMail } from 'react-icons/ai';
 import { AiOutlinePhone } from 'react-icons/ai';
 import { FiFacebook } from 'react-icons/fi';
@@ -8,11 +10,19 @@ import { FaTiktok } from 'react-icons/fa';
 function ContactBar() {
   return (
     <div className={styles.contact_bar}>
-      <AiOutlineMail className={styles.icon} />
-      <AiOutlinePhone className={styles.icon} />
-      <FiFacebook className={styles.icon} />
-      <AiOutlineInstagram className={styles.icon} />
-      <FaTiktok className={styles.icon} />
+      <div className={styles.contact_bar__container}>
+        <div className={styles.logo__container}>
+          <Image src={Logo} width={50} height={50} alt='logo' />
+          <h2 className={styles.logo__text}>BoomBalloonLV</h2>
+        </div>
+        <div className={styles.icons__container}>
+          <AiOutlineMail className={styles.icon} />
+          <AiOutlinePhone className={styles.icon} />
+          <FiFacebook className={styles.icon} />
+          <AiOutlineInstagram className={styles.icon} />
+          <FaTiktok className={styles.icon} />
+        </div>
+      </div>
     </div>
   );
 }
